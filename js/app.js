0988d9d42763a0168f49868ee84100da88f5a7c1
naveenkumar061 <devarashettynaveen224@gmail.com>
2023-09-16 15:29:25 +0530

    About session and some modification reg nav and header is completed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
+const links = document.querySelectorAll("#sidebar-links a");
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
-
+const date = document.querySelector("#date");
+// show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+  });
+});
+// set year
+date.innerHTML = new Date().getFullYear();
